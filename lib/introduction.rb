25a16cd99f2b65ee708b2d84aa0b4f22dbb51858
Belinda Lei <belinda.lei95@gmail.com>
2019-05-31 01:12:13 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,5 +5,5 @@ def introduction(name)
 end 
 
 def introduction_with_language(name,language)
-  puts "Hello #{name}. Do you know #{language}?"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
